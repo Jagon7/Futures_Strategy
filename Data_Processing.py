26ab9bd3cc006e9963db7af6579d082024ab6a2a
Jagon <jason90413@gmail.com>
2024-01-01 21:19:32 +0800

    make function

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,47 +1,49 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from datetime import datetime as dt
+from datetime import datetime, time, timedelta
 
-data = pd.read_csv('Data/taiwanfuture_all_TX_second.csv', encoding="GB18030")
+def preprocess_data(DUE_MONTH, FILE_PATH='Data/taiwanfuture_all_TX_second.csv'):
+    # Read data
+    data = pd.read_csv(FILE_PATH, encoding="GB18030")
 
-data['DueMonth'] = data['DueMonth'].str.strip()
-data['Time'] = data['Time'].astype(str)
-data = data[data['DueMonth'] == '202312'].reset_index(drop=True)
+    # Strip white spaces and filter by due_month
+    data['DueMonth'] = data['DueMonth'].str.strip()
+    data['Time'] = data['Time'].astype(str)
+    data = data[data['DueMonth'] == DUE_MONTH].reset_index(drop=True)
 
-# make time format consistent
-data['Time'] = data['Time'].apply(lambda x: '0' * (6 - len(x)) + x if len(x) < 6 else x)
-data['Date'] = pd.to_datetime(data['Date'], format='%Y%m%d')
-data['Time'] = pd.to_datetime(data['Time'], format='%H%M%S').dt.time
+    # Make time format consistent
+    data['Time'] = data['Time'].apply(lambda x: '0' * (6 - len(x)) + x if len(x) < 6 else x)
 
-# make datetime index
-data['datetime'] = data['Date'] + pd.to_timedelta(data['Time'].astype(str))
-data.set_index('datetime', inplace=True)
-data.drop(['Date', 'Time', 'DueMonth'], axis=1, inplace=True)
+    # Convert 'Date' and 'Time' to datetime
+    data['Date'] = pd.to_datetime(data['Date'], format='%Y%m%d')
+    data['Time'] = pd.to_datetime(data['Time'], format='%H%M%S').dt.time
 
-# resample data into 1 minute interval
-data = data.resample('1Min').agg({'Price':'ohlc', 'Volume':'sum'})
+    # Create datetime index
+    data['datetime'] = data['Date'] + pd.to_timedelta(data['Time'].astype(str))
+    data.set_index('datetime', inplace=True)
+    data.drop(['Date', 'Time', 'DueMonth'], axis=1, inplace=True)
 
-# Split data strategy into two parts: In-hours and After-hours
-# Define the time range
-in_hours_start_time = pd.to_datetime('08:45:00').time()
-in_hours_end_time = pd.to_datetime('13:45:00').time()
-after_hours_start_time = pd.to_datetime('15:00:00').time()
-after_hours_end_time = pd.to_datetime('05:00:00').time()
+    # Resample data into 1 minute interval
+    data = data.resample('1Min').agg({'Price': 'ohlc', 'Volume': 'sum'})
 
-# Create boolean masks for the time ranges
-in_hours = (data.index.time >= in_hours_start_time) & (data.index.time <= in_hours_end_time)
-after_hours = (data.index.time >= after_hours_start_time) | (data.index.time <= after_hours_end_time)
+    # Split data into in-hours and after-hours
+    in_hours_start_time = time(8, 45, 0)
+    in_hours_end_time = time(13, 45, 0)
+    after_hours_start_time = time(15, 0, 0)
+    after_hours_end_time = time(5, 0, 0)
 
-df_in_hours = data[in_hours]
-df_after_hours = data[after_hours]
+    in_hours = (data.index.time >= in_hours_start_time) & (data.index.time <= in_hours_end_time)
+    after_hours = (data.index.time >= after_hours_start_time) | (data.index.time <= after_hours_end_time)
 
-# Convert multi-index to single index
-df_in_hours.columns = [col[1] for col in df_in_hours.columns.values]
-df_after_hours.columns = [col[1] for col in df_after_hours.columns.values]
+    df_in_hours = data[in_hours]
+    df_after_hours = data[after_hours]
 
-# Fill NaN values using forward-fill
-na_cols = ['open', 'high', 'low', 'close']
-df_in_hours[na_cols] = df_in_hours[na_cols].fillna(method='ffill', inplace=False)
-df_after_hours[na_cols] = df_after_hours[na_cols].fillna(method='ffill', inplace=False)
+    # Convert multi-index to single index
+    df_in_hours.columns = [col[1] for col in df_in_hours.columns.values]
+    df_after_hours.columns = [col[1] for col in df_after_hours.columns.values]
+
+    # Fill NaN values using forward-fill
+    na_cols = ['open', 'high', 'low', 'close']
+    df_in_hours[na_cols] = df_in_hours[na_cols].fillna(method='ffill', inplace=False)
+    df_after_hours[na_cols] = df_after_hours[na_cols].fillna(method='ffill', inplace=False)
+
+    return df_in_hours, df_after_hours
